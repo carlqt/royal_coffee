eaa3faf4b194e26bc1a935f35c0f2711f188c29e
Carl Tablante <carl.q.tablante@gmail.com>
2016-01-31 11:08:15 +0800

    grouping by type and size

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,4 +19,10 @@ class Product < ActiveRecord::Base
   validates :tall_price, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
   enum drink_type: { coffee: 0, tea: 1 }
 
+  scope :by_drink_type, ->(type) { where(drink_type: type) }
+  scope :by_size, ->(size) { where.not("#{size}" => nil) }
+
+  def self.available_sizes
+    column_names.select{ |name| name.match("price") }
+  end
 end
